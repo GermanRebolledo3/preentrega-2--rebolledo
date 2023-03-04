1820af033502eb8307167d6857fbcc21dc36127b
GermanRebolledo <germigallina@gmail.com>
2023-03-04 14:03:14 -0300

    entrega con js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,152 @@
-let lista_alumnos = [];
-
-    let entrada = parseInt(prompt("Escriba el numero para seleccionar la opcion: \n 1. Ver la lista de alumnos \n 2. Agregar un alumno a la lista \n 3. Buscar a un alumno de la lista \n 4. Eliminar a todos los alumnos cargados \n 0. Salir"))
+//form de validacion
+function validateform(){
+    var nombre = document.getElementById("nombre").value;
+    var edad = document.getElementById("edad").value;
+    var direccion = document.getElementById("direccion").value;
+    var mail = document.getElementById("mail").value;
     
-    while(entrada != 0){
-        switch(entrada){
-            case 1:
-                if (lista_alumnos.length <= 0){
-                    alert("No hay alumnos cargados")
-                }
-                else if (lista_alumnos.length > 0){
-                    alert("Los alumnos son:" + " " +lista_alumnos)
-                }
-                break;
-            case 2:
-                let alumno = prompt("agregar un alumno");
-                lista_alumnos.push(alumno);
-                break;
-            case 3:
-                let buscar = prompt("Busca a un alumno");
-                let encontrar = lista_alumnos.filter(item => item.includes(buscar))
-                let siexiste = lista_alumnos.some(item => item.includes(buscar))
-                if(siexiste === true){
-                    alert("alumno encontrado:" + " " + encontrar)
-                }
-                else if(siexiste === false) {
-                    alert("Alumno no encontrado");
-                }
-                break;
-            case 4:
-                lista_alumnos.length = 0;
-                break;
-            default:
-                alert("Seleccion erronea");
-                break;
-        }
-        entrada = parseInt(prompt("Escriba el numero para seleccionar la opcion: \n 1. Ver la lista de alumnos \n 2. Agregar un alumno a la lista \n 3. Buscar a un alumno de la lista \n 4. Eliminar a todos los alumnos cargados \n 0. Salir"))
+if(nombre == ""){
+    alert("Ingrese un nombre")
+    return false;
+}
+if(edad == ""){
+    alert("Ingrese una edad")
+    return false;
+}
+else if (edad < 1){
+    alert("La edad no puede ser menor de 1 año")
+    return false;
+}
+if (direccion ==""){
+    alert("Indique su direccion")
+    return false;
+}
+if (mail ==""){
+    alert("Ingrese su email")
+    return false;
+}else if(!mail.includes("@")){
+    alert("Ingrese un mail valido")
+    return false;
+}
+return true;
+}
+//function para mostrar la data
+function showData(){
+    var lista ;
+    
+    if(localStorage.getItem("lista")== null){
+        lista = [];
     }
+    else{
+        lista = JSON.parse(localStorage.getItem("lista"))
+    }
+    var html = "";
+
+    lista .forEach( function(element, index )  {
+        html += "<tr>"
+        html += "<td>" + element.nombre + "<td>";
+        html += "<td>" + element.edad + "<td>";
+        html += "<td>" + element.mail + "<td>";
+        html += "<td>" + element.direccion + "<td>";
+        html += '<td>  <button onclick="deleteData ('+ index +')"class="btn btn-danger">Delete</button> <button onclick="updateData ('+ index +')"class="btn btn-warning m-2">Editar</button></td>';
+       
+        html +="</tr>";
+        
+    });
+    document.querySelector("#TablaDeResultados tbody").innerHTML = html;
+}
+ 
+document.onload = showData();
+
+//function para añadir data
+
+
+function addData(){
+    
+    if(validateform() == true){
+        var nombre = document.getElementById("nombre").value;
+        var edad = document.getElementById("edad").value;
+        var direccion = document.getElementById("direccion").value;
+        var mail = document.getElementById("mail").value;
+
+        var lista;
+        if(localStorage.getItem("lista") == null){
+            lista = [];
+        }else {
+            lista = JSON.parse(localStorage.getItem("lista"));
+        }
+        lista.push({
+            nombre: nombre ,
+            edad: edad ,
+            direccion: direccion ,
+            mail: mail ,
+        });
+
+        localStorage.setItem("lista",JSON.stringify(lista));
+        showData();
+        document.getElementById("nombre").value = "";
+        document.getElementById("edad").value = "";
+        document.getElementById("direccion").value = "";
+        document.getElementById("mail").value = "";
+    }
+}
+
+//function para eliminar data del local
+
+function deleteData (index){
+    var lista;
+    if(localStorage.getItem("lista")== null){
+        lista = [];
+    }
+    else{
+        lista = JSON.parse(localStorage.getItem("lista"))
+    }
+    lista.splice(index, 1);
+    localStorage.setItem("lista", JSON.stringify(lista));
+    showData();
+}
+
+//function para editar data
+
+function updateData(index){
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("update").style.display = "block";
+   
+    var lista;
+    if(localStorage.getItem("lista")== null){
+        lista = [];
+    }
+    else{
+        lista = JSON.parse(localStorage.getItem("lista"))
+    }
+    lista.splice(index, 1);
+    localStorage.setItem("lista", JSON.stringify(lista));
+    showData();
+
+    document.getElementById("nombre").value = lista[index].nombre;
+    document.getElementById("edad").value = lista[index].edad;
+    document.getElementById("direccion").value = lista[index].direccion;
+    document.getElementById("mail").value = lista[index].mail;
+
+    document.querySelector("#update").onclick = function()
+{
+if(validateform()== true){
+    lista[index].nombre = document.getElementById("nombre").value;
+    lista[index].edad = document.getElementById("edad").value;
+    lista[index].direccion = document.getElementById("direccion").value;
+    lista[index].mail = document.getElementById("mail").value;
+
+    localStorage.setItem("lista", JSON.stringify(lista));
+    showData();
+    
+    document.getElementById("nombre").value = "";
+    document.getElementById("edad").value = "";
+    document.getElementById("direccion").value = "";
+    document.getElementById("mail").value = "";
+    
+    
+        document.getElementById("submit").style.display = "block";
+        document.getElementById("update").style.display = "none";
+}
+} 
+}
